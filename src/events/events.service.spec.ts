48e4b4930dc9f337fc16137a0c2a9cc2717e6536
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2024-01-12 12:24:10 +0600

    add test with pagination

--- src/events/events.service.spec.ts
+++ src/events/events.service.spec.ts
@@ -3,10 +3,14 @@ import { EventService } from './events.service'
 import { Event } from './event.entity'
 import { Test } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
+import * as paginator from './../pagination/paginator'
+
+jest.mock('./../pagination/paginator')
 
 describe('EventsService', () => {
   let service: EventService
   let repository: Repository<Event>
+  let mockedPaginated
 
   const deleteQb = {
     where: jest.fn(),
@@ -21,6 +25,7 @@ describe('EventsService', () => {
   }
 
   beforeEach(async () => {
+    mockedPaginated = paginator.paginate as jest.Mock
     const module = await Test.createTestingModule({
       providers: [
         EventService,
@@ -71,6 +76,56 @@ describe('EventsService', () => {
       expect(whereSpy).toHaveBeenCalledTimes(1)
       expect(whereSpy).toHaveBeenCalledWith('id = :id', { id: 1 })
       expect(executeSpy).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('getEventsAttendedByUserIdPaginated', () => {
+    it('should return a list of paginated events', async () => {
+      const orderBySpy = jest
+        .spyOn(selectQb, 'orderBy')
+        .mockReturnValue(selectQb)
+      const leftJoinSpy = jest
+        .spyOn(selectQb, 'leftJoinAndSelect')
+        .mockReturnValue(selectQb)
+      const whereSpy = jest.spyOn(selectQb, 'where').mockReturnValue(selectQb)
+
+      mockedPaginated.mockResolvedValue({
+        first: 1,
+        last: 1,
+        total: 10,
+        limit: 10,
+        data: [],
+      })
+
+      expect(
+        service.getEventsAttendedByUserIdPaginated(500, {
+          limit: 1,
+          currentPage: 1,
+        }),
+      ).resolves.toEqual({
+        data: [],
+        first: 1,
+        last: 1,
+        limit: 10,
+        total: 10,
+      })
+
+      expect(orderBySpy).toHaveBeenCalledTimes(1)
+      expect(orderBySpy).toHaveBeenCalledWith('e.id', 'DESC')
+
+      expect(leftJoinSpy).toHaveBeenCalledTimes(1)
+      expect(leftJoinSpy).toHaveBeenCalledWith('e.attendees', 'a')
+
+      expect(whereSpy).toHaveBeenCalledTimes(1)
+      expect(whereSpy).toHaveBeenCalledWith('a.userId = : userId', {
+        userId: 500,
+      })
+
+      expect(mockedPaginated).toHaveBeenCalledTimes(1)
+      expect(mockedPaginated).toHaveBeenCalledWith(selectQb, {
+        currentPage: 1,
+        limit: 1,
+      })
     })
   })
 })
